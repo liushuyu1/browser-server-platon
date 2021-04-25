4c4f218cac74cef6e65b39bf62e9fd2d0a5aa7a7
HYPWolf <hyp1573796294@gmail.com>
2021-04-25 17:12:14 +0800

    拦截异常的SQL并打印

--- scan-service/src/main/java/com/platon/browser/config/mysql/MybatisPlusConfig.java
+++ scan-service/src/main/java/com/platon/browser/config/mysql/MybatisPlusConfig.java
@@ -1,14 +1,16 @@
 package com.platon.browser.config.mysql;
 
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.platon.browser.interceptor.SqlInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class MybatisPlusConfig {
+
 
     /**
      * 分页插件
@@ -19,6 +21,7 @@ public class MybatisPlusConfig {
      * @date 2021/4/8
      */
     @Bean
+    @Order(1)
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
@@ -28,5 +31,18 @@ public class MybatisPlusConfig {
         return interceptor;
     }
 
+    /**
+     * SQL拦截器
+     *
+     * @param
+     * @return com.platon.browser.interceptor.MyBatisPlusInterceptor
+     * @author huangyongpeng@matrixelements.com
+     * @date 2021/4/25
+     */
+    @Bean
+    @Order(2)
+    public SqlInterceptor myBatisInterceptor() {
+        return new SqlInterceptor();
+    }
 
 }
